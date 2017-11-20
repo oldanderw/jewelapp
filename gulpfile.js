129db67c9c66c13749c8c5f2ed9e2df5f363e8a5
David parker <adparker@fullsail.edu>
2017-11-20 14:26:43 -0500

    added a the building proess in gulp to speed up thnig and added plugin(s)

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,34 @@
-var gulp = require('gulp');
-var gutil = require('gulp-util');
-var bower = require('bower');
-var concat = require('gulp-concat');
-var sass = require('gulp-sass');
-var minifyCss = require('gulp-minify-css');
-var rename = require('gulp-rename');
-var sh = require('shelljs');
+const gulp = require('gulp');
+const gutil = require('gulp-util');
+const bower = require('bower');
+const concat = require('gulp-concat');
+const sass = require('gulp-sass');
+const minifyCss = require('gulp-minify-css');
+const rename = require('gulp-rename');
+const shell = require('gulp-shell');
 
-var paths = {
+const paths = {
   sass: ['./scss/**/*.scss']
 };
 
-gulp.task('default', ['scripts', 'templates', 'sass', 'assets', 'fonts']);
-gulp.task('scripts', function (done) {
-  gulp.src('app/scripts/**/*', { base: 'app/scripts'} )
+gulp.task('default', ['scripts', 'templates', 'sass', 'assets', 'fonts', 'abuild']);
+
+gulp.task('scripts', function(done) {
+  gulp.src('app/scripts/**/*', {base: 'app/scripts'})
   .pipe(gulp.dest('www/scripts'))
   .on('end', done);
 });
 
-gulp.task('fonts', function (done) {
-  gulp.src('app/fonts/**/*', { base: 'app/fonts'} )
+gulp.task('fonts', function(done) {
+  gulp.src('app/fonts/**/*', {base: 'app/fonts'})
     .pipe(gulp.dest('www/fonts'))
     .on('end', done);
 });
 
-gulp.task('templates', function (done) {
+gulp.task('templates', function(done) {
   gulp.src('./app/index.html')
     .pipe(gulp.dest('www'));
-  gulp.src('app/templates/**/*', { base: 'app/templates'})
+  gulp.src('app/templates/**/*', {base: 'app/templates'})
     .pipe(gulp.dest('www/templates'))
     .on('end', done);
 });
@@ -43,14 +44,17 @@ gulp.task('sass', function(done) {
 //   .pipe(gulp.dest('www/css/'))
     .on('end', done);
 });
-gulp.task('assets', function (done) {
+gulp.task('assets', function(done) {
   gulp.src('app/images/*.*').pipe(gulp.dest('www/images')).on('end', done);
 });
+
 gulp.task('watch', function() {
   gulp.watch(paths.sass, ['sass']);
   gulp.watch(paths.templates, ['templates']);
   gulp.watch(paths.scripts, ['scripts']);
 });
+
+gulp.task('abuild', shell.task('ionic cordova build android'));
 
 gulp.task('install', ['git-check'], function() {
   return bower.commands.install()
